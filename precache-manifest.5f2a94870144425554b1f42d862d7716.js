d4919632b895f952b1417e621d0be24f231f72cb
khabiirk <khabiir.khodabaccus@mail.mcgill.ca>
2020-07-19 21:47:03 -0400

    Updates

---
+++ precache-manifest.5f2a94870144425554b1f42d862d7716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424ec4c36f36f263195f9c74f64b2610",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "26d2b50b1a6094e0144b",
+    "url": "/Weather/static/css/main.861808f9.chunk.css"
+  },
+  {
+    "revision": "3962a70ae60ad69a4c08",
+    "url": "/Weather/static/js/2.85d78dbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.85d78dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d2b50b1a6094e0144b",
+    "url": "/Weather/static/js/main.5d038048.chunk.js"
+  },
+  {
+    "revision": "33b8acc232781a3d41c0",
+    "url": "/Weather/static/js/runtime-main.22be1482.js"
+  }
+]);
